c8949f888270a938ca47ffeea9615f5901326c86
slykid <slykid@naver.com>
2021-02-08 21:59:10 +0900

    Java Basic: 24. Generic Programming

---
+++ Java/src/com/java/kilhyun/OOP/UtilPair.java
@@ -0,0 +1,11 @@
+package com.java.kilhyun.OOP;
+
+public class UtilPair {
+    public static <K, V> boolean compare(Pair<K, V> p1, Pair<K, V> p2)
+    {
+        boolean keyCompare = p1.getKey().equals(p2.getKey());
+        boolean valueCompare = p1.getValue().equals(p2.getValue());
+
+        return keyCompare && valueCompare;
+    }
+}
