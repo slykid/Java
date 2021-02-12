0c475a5a54acad6ae95b6b3ddb65100bd7cdbc4e
slykid <slykid@naver.com>
2021-02-12 23:50:35 +0900

    Java Basic: 25. Collection : List

---
+++ Java/src/com/java/kilhyun/OOP/ex24_3_LinkedListCollectionTest.java
@@ -0,0 +1,36 @@
+package com.java.kilhyun.OOP;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ex24_3_LinkedListCollectionTest {
+
+    public static void main(String[] args)
+    {
+        List<String> linkedList1 = new LinkedList<String>();
+        List<String> linkedList2 = new LinkedList<String>();
+
+        long startTime;
+        long endTime;
+
+        startTime = System.nanoTime();
+        for(int i = 0; i < 10000; i++)
+        {
+            linkedList1.add(0, String.valueOf(i));
+        }
+
+        endTime = System.nanoTime();
+
+        System.out.println("ArrayList 걸린시간: " + (endTime - startTime) + " ns");
+
+        startTime = System.nanoTime();
+        for(int i = 0; i < 10000; i++)
+        {
+            linkedList2.add(0, String.valueOf(i));
+        }
+        endTime = System.nanoTime();
+        System.out.println("LinkedList 걸린시간: " + (endTime - startTime) + " ns");
+
+    }
+
+}
