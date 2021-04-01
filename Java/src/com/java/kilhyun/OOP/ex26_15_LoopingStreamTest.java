3b2a706c8ed371d41841246671873346a3cb11dd
kilhyunkim <slykid@naver.com>
2021-04-01 13:07:01 +0900

    Java Basic: 27. Stream

---
+++ Java/src/com/java/kilhyun/OOP/ex26_15_LoopingStreamTest.java
@@ -0,0 +1,20 @@
+package com.java.kilhyun.OOP;
+
+public class ex26_15_LoopingStreamTest {
+    /*
+        루핑(Looping)
+        - 요소전체를 반복하는 것
+
+        - 주요 메소드
+          - peek() : 중간 처리 시, 전체 요소에 대해 추가 작업을 하기 위해서 사용함
+          - forEach() : 전체 요소에 대한 처리를 하는 메소드 / 요소를 소비하기 때문에, 이 후에 sum() 등의 함수사용불가
+
+     */
+
+
+    public static void main(String[] args)
+    {
+
+    }
+
+}
