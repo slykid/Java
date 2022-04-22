6962efc4985a2e0630fb7f887e5c57e25377f030
slykid <slykid@naver.com>
2022-04-22 17:38:54 +0900

    [Spring] 8. Object Mapper

--- Spring/HelloSpringBoot/src/test/java/com/kilhyun/study/hellospringboot/HelloSpringBootApplicationTests.java
+++ Spring/HelloSpringBoot/src/test/java/com/kilhyun/study/hellospringboot/HelloSpringBootApplicationTests.java
@@ -1,5 +1,8 @@
 package com.kilhyun.study.hellospringboot;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kilhyun.study.hellospringboot.dto.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +10,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 class HelloSpringBootApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws JsonProcessingException {
+        System.out.println("------------------------");
+
+        var objectMapper = new ObjectMapper();
+
+        // Object to text
+        var user = new User("slykid", 30);
+        var text = objectMapper.writeValueAsString(user);
+        System.out.println(text);
+
     }
 
 }
