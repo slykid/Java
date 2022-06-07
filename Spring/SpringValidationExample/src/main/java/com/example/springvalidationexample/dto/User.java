a8c7a646d0f246bad7eea732ee1c28a94b50ef5b
slykid <slykid@naver.com>
2022-06-07 22:19:38 +0900

    [Spring] 11. Spring Validation

--- Spring/SpringValidationExample/src/main/java/com/example/springvalidationexample/dto/User.java
+++ Spring/SpringValidationExample/src/main/java/com/example/springvalidationexample/dto/User.java
@@ -1,9 +1,8 @@
 package com.example.springvalidationexample.dto;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class User {
 
@@ -18,6 +17,10 @@ public class User {
 
     @Pattern(regexp="^[0-9]{3}-[0-9]{3,4}-[0-9]{4}$", message="전화번호 형식이 아닙니다.")
     private String phoneNumber;
+
+    @Size(min=6, max=6)   // 길이는 만족할 수 있지만, 111111 과 같이 yyyymm 이 아닌 형식에도 통과할 수 있음
+    private String requestDate;  // 형식: yyyymm
+
 
     public String getName() {
         return name;
@@ -49,6 +52,28 @@ public class User {
 
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
+    }
+
+    public String getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(String requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    @AssertTrue(message = "YYYYMM 형식에 맞지 않습니다.")
+    public boolean isRequestDateValidation() {
+
+        // 앞서 길이 검증으로 인해 발생할 수 있는 에러에 대한 별도 검증 로직 구현
+        try {
+            LocalDate localDate = LocalDate.parse(getRequestDate() + "01", DateTimeFormatter.ofPattern("yyyyMMdd"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
     }
 
     @Override
