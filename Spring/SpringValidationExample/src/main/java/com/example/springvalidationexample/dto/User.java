c97c78b2652ac4a3aca46be4f62625985e38be72
slykid <slykid@naver.com>
2022-06-07 20:39:11 +0900

    [Spring] 11. Spring Validation

--- Spring/SpringValidationExample/src/main/java/com/example/springvalidationexample/dto/User.java
+++ Spring/SpringValidationExample/src/main/java/com/example/springvalidationexample/dto/User.java
@@ -1,10 +1,22 @@
 package com.example.springvalidationexample.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 public class User {
 
+    @NotBlank
     private String name;
+
+    @Min(0)
     private int age;
+
+    @Email
     private String email;
+
+    @Pattern(regexp="^[0-9]{3}-[0-9]{3,4}-[0-9]{4}$", message="전화번호 형식이 아닙니다.")
     private String phoneNumber;
 
     public String getName() {
