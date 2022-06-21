f3c91c01351e54e615c3688baf6505a9c6954d20
slykid <slykid@naver.com>
2022-06-21 20:14:08 +0900

    [Spring] 12. Spring Exception

---
+++ Spring/SpringBootExceptions/src/main/java/com/example/springbootexceptions/advice/GlobalControllerAdvice.java
@@ -0,0 +1,30 @@
+package com.example.springbootexceptions.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(basePackages = "com.example.springbootexceptions.controller")
+public class GlobalControllerAdvice {
+
+    // 방법 1. 발생하는 모든 Exception 에 대한 처리
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity exception(Exception e) {
+
+        System.out.println("-------------------------------------------------");
+        System.out.println(e.getLocalizedMessage());  // 별도로 지정 가능함
+        System.out.println("-------------------------------------------------");
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("");
+    }
+
+    // 방법 2. 특정 메소드에서 발생한 Exception 별로 처리
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity MethodArgumentNotValidException(MethodArgumentNotValidException e) {
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+}
--- Spring/SpringBootExceptions/src/main/java/com/example/springbootexceptions/controller/RestApiController.java
+++ Spring/SpringBootExceptions/src/main/java/com/example/springbootexceptions/controller/RestApiController.java
@@ -1,6 +1,9 @@
 package com.example.springbootexceptions.controller;
 
 import com.example.springbootexceptions.dto.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -27,6 +30,14 @@ public class RestApiController {
         System.out.println(user);
 
         return user;
+    }
+
+    // 방법 3. 해당 컨트롤러에 추가하기
+    // - GlobalControllerAdvice 로 잡혀있더라도 우선 순위는 실행된 컨트롤러 내의 ExceptionHandler 가 더 높다.
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity MethodArgumentNotValidException(MethodArgumentNotValidException e) {
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
 }
