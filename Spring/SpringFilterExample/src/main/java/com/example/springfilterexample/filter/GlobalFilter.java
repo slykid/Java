69db72b97b8712a1435b7dfb232fa03857e69ca0
slykid <slykid@naver.com>
2022-08-06 22:09:00 +0900

    [Spring] 13. Spring Filter

--- Spring/SpringFilterExample/src/main/java/com/example/springfilterexample/filter/GlobalFilter.java
+++ Spring/SpringFilterExample/src/main/java/com/example/springfilterexample/filter/GlobalFilter.java
@@ -3,6 +3,7 @@ package com.example.springfilterexample.filter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
@@ -19,17 +20,29 @@ public class GlobalFilter implements Filter {
 
         // 전처리
 //        HttpServletRequest httpServletRequest = (HttpServletRequest)request;   // getReader 부분에서 에러 발생 → read() 는 한번만 발생하기 때문에, 이미 읽어버려서 읽을 내용이 없기 때문에 발생함
-        ContentCachingRequestWrapper httpServletRequest = (HttpServletRequest)request;
-        HttpServletResponse httpServletResponse = (HttpServletResponse)response;
+        ContentCachingRequestWrapper httpServletRequest = new ContentCachingRequestWrapper((HttpServletRequest)request);
+//        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        ContentCachingResponseWrapper httpServletResponse = new ContentCachingResponseWrapper((HttpServletResponse)response);
 
         String url = httpServletRequest.getRequestURI();
 
-        BufferedReader bufferedReader = httpServletRequest.getReader();
-        bufferedReader.lines().forEach(line -> {
-            log.info("URL: {}, line: {}", url, line);
-        });
+//        BufferedReader bufferedReader = httpServletRequest.getReader();
+//        bufferedReader.lines().forEach(line -> {
+//            log.info("URL: {}, line: {}", url, line);
+//        });
+
 
         chain.doFilter(httpServletRequest, httpServletResponse);
+
+        // 후처리(ContentCaching 사용 시, 위의 BufferedReader 에서 수행하는 역할을 아래에서 해야됨)
+        // - request
+        String reqContent = new String(httpServletRequest.getContentAsByteArray());
+        log.info("Request URL: {}, Request Body: {}", url, reqContent);
+
+        String resContent = new String(httpServletResponse.getContentAsByteArray());
+        int httpStatus = httpServletResponse.getStatus();
+
+        log.info("Response status : {}, Response Body: {}", httpStatus, resContent);
 
     }
 }
