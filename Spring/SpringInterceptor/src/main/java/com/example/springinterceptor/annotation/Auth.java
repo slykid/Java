c809f601e0c62e60a7b9d98e3d696ca0b2f17618
slykid <slykid@naver.com>
2022-08-26 15:53:37 +0900

    [Spring] 14. SpringInterceptor

---
+++ Spring/SpringInterceptor/src/main/java/com/example/springinterceptor/annotation/Auth.java
@@ -0,0 +1,11 @@
+package com.example.springinterceptor.annotation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+public @interface Auth {
+
+
+}
---
+++ Spring/SpringInterceptor/src/main/java/com/example/springinterceptor/exception/AuthException.java
@@ -0,0 +1,5 @@
+package com.example.springinterceptor.exception;
+
+
+public class AuthException extends RuntimeException{
+}
