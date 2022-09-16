b8c3fafac226b8d2316a42f6d31a2b41cb4aad8e
slykid <slykid@naver.com>
2022-09-16 22:01:31 +0900

    Update '22.09.16

---
+++ Spring/SpringFilterExample/src/main/java/com/example/springfilterexample/SpringFilterExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springfilterexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFilterExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFilterExampleApplication.class, args);
+    }
+
+}
