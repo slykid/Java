eceb47f895ec602f3b3372afb6576c7fb1779634
slykid <slykid@naver.com>
2023-07-12 17:33:46 +0900

    [Spring] 19. JUnit

--- Spring/JUnitTest/src/test/java/DollarCalculatorTest.java
+++ Spring/JUnitTest/src/test/java/DollarCalculatorTest.java
@@ -1,8 +1,27 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+/*
+mocking 처리
+- 특정한 객체가 특정 메소드에 의해 호출되었을 때 정확한 값이 나오는 지를 확인하기 위한 처리
+ */
+
+@ExtendWith(MockitoExtension.class)
 public class DollarCalculatorTest {
+
+    @Mock
+    public MarketApi marketApi;
+
+    @BeforeEach
+    public void init() {
+        Mockito.lenient().when(marketApi.connect()).thenReturn(3000);
+    }
 
     @Test
     public void testHello(){
@@ -13,7 +32,7 @@ public class DollarCalculatorTest {
     @Test
     public void dollarTest()
     {
-        MarketApi marketApi = new MarketApi();
+        MarketApi marketApi = new MarketApi(); // mocking 처리대상
         DollarCalculator dollarCalculator = new DollarCalculator(marketApi);
         dollarCalculator.init();
 
@@ -25,5 +44,19 @@ public class DollarCalculatorTest {
         Assertions.assertEquals(0, calculator.minus(10, 10));
     }
 
+    @Test
+    public void mockingTest()
+    {
+
+        DollarCalculator dollarCalculator = new DollarCalculator(marketApi);
+        dollarCalculator.init();
+
+        Calculator calculator = new Calculator(dollarCalculator);
+
+        System.out.println(calculator.sum(10 , 10));
+
+        Assertions.assertEquals(60000, calculator.sum(10, 10));
+        Assertions.assertEquals(0, calculator.minus(10, 10));
+    }
 
 }
