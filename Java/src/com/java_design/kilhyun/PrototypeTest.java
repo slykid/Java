9cdbf56d1821b748aa218f1ce7884a425ee3879c
slykid <slykid@naver.com>
2023-12-16 23:27:31 +0900

    fix: [Java Design Pattern] Renewal '23.12.16

---
+++ Java/src/com/java_design/kilhyun/PrototypeTest.java
@@ -0,0 +1,11 @@
+package com.java_design.kilhyun;
+
+public class PrototypeTest
+{
+
+    public static void main(String[] args)
+    {
+
+    }
+
+}
---
+++ Java/src/com/java_design/kilhyun/SingletonTest.java
@@ -0,0 +1,12 @@
+package com.java_design.kilhyun;
+
+public class SingletonTest
+{
+
+    public static void main(String[] args)
+    {
+
+
+    }
+
+}
