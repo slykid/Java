78e00b93fbf98cfcc25bd5a87999707c7180a3f6
slykid <slykid@naver.com>
2024-04-06 10:46:48 +0900

    [Spring] 22. Swagger

--- Spring/Swagger/src/main/java/org/example/swagger/controller/ApiController.java
+++ Spring/Swagger/src/main/java/org/example/swagger/controller/ApiController.java
@@ -1,9 +1,10 @@
 package org.example.swagger.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.*;
 
+@Tag(name="테스트", description="API 정보를 제공하는 Controller")
 @RestController
 @RequestMapping("/api")
 public class ApiController {
@@ -13,5 +14,15 @@ public class ApiController {
         return "Hello Swagger!";
     }
 
+    @GetMapping("/plus/{x}")
+    public int plus(
+            @Parameter(name="x값", description="x 값", example="20")
+            @PathVariable int x,
+            @Parameter(name="y값", description="y 값", example="5")
+            @RequestParam int y
+    )
+    {
+        return x + y;
+    }
 
 }
