1df1f56fd6bcbbf2952089b40ec2dcae480b780c
slykid <slykid@naver.com>
2024-04-26 20:08:22 +0900

    [Spring] 22. Swagger

--- Spring/Swagger/src/main/java/org/example/swagger/controller/ApiController.java
+++ Spring/Swagger/src/main/java/org/example/swagger/controller/ApiController.java
@@ -1,7 +1,10 @@
 package org.example.swagger.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.swagger.dto.UserReq;
 import org.example.swagger.dto.UserRes;
@@ -17,17 +20,29 @@ public class ApiController {
         return "Hello Swagger!";
     }
 
+//    @GetMapping("/plus/{x}")
+//    public int plus(
+//            @PathVariable
+//            @Schema(description="Path Value", example="20") int x,
+//            @Parameter(name="y", description="y값 / Query Parameter", example="5")
+//            @RequestParam int y
+//    )
+//    {
+//        return x + y;
+//    }
+
+    @Parameters({
+        @Parameter(name="x", description="x 값"),
+        @Parameter(name="y", description="y 값")
+    })
     @GetMapping("/plus/{x}")
-    public int plus(
-            @PathVariable
-            @Schema(description="Path Value", example="20") int x,
-            @Parameter(name="y", description="y값 / Query Parameter", example="5")
-            @RequestParam int y
-    )
+    public int plus(@PathVariable int x, @RequestParam int y)
     {
         return x + y;
     }
 
+    @ApiResponse(responseCode="600", description="사용자의 나이가 10살 이하일 때")
+    @Operation(summary="사용자의 이름과 나이를 리턴하는 메소드")
     @GetMapping("/user")
     public UserRes user(UserReq userReq)
     {
--- Spring/Swagger/src/main/java/org/example/swagger/dto/UserRes.java
+++ Spring/Swagger/src/main/java/org/example/swagger/dto/UserRes.java
@@ -1,5 +1,6 @@
 package org.example.swagger.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserRes {
 
+    @Schema(description="사용자의 이름", example="slykid", required = true)
     private String name;
+
+    @Schema(description="사용자의 나이", example="30", required = true)
     private int age;
 
 }
