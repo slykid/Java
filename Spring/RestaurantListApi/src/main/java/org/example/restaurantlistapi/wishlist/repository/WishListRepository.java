8c247c3a7feb1ba309d00f2f40fa2516a3ea77db
slykid <slykid@naver.com>
2024-05-06 12:31:56 +0900

    [Spring] Project: 네이버맛집리스트 API 개발 * 메모리 DB 생성 완료 * 네이버 검색 API (지역, 이미지) REST로 호출 완료

---
+++ Spring/RestaurantListApi/src/main/java/org/example/restaurantlistapi/wishlist/repository/WishListRepository.java
@@ -0,0 +1,10 @@
+package org.example.restaurantlistapi.wishlist.repository;
+
+import org.example.restaurantlistapi.db.MemoryDbRepositoryAbstract;
+import org.example.restaurantlistapi.wishlist.entity.WishListEntity;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class WishListRepository extends MemoryDbRepositoryAbstract<WishListEntity> {
+
+}
