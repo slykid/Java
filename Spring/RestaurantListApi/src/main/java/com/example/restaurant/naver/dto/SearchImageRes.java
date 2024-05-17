00685bb280b278322d67825658f9aa6942de9b0a
slykid <slykid@naver.com>
2024-05-17 23:22:03 +0900

    [Spring] Project: 네이버맛집리스트 API 개발 * 경로변경

---
+++ Spring/RestaurantListApi/src/main/java/com/example/restaurant/naver/dto/SearchImageRes.java
@@ -0,0 +1,30 @@
+package com.example.restaurant.naver.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class SearchImageRes {
+    private String lastBuildDate;
+    private int total;
+    private int start;
+    private int display;
+    private List<SearchLocalItem> items;
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Data
+    public static class SearchLocalItem {
+        private String title;
+        private String link;
+        private String thumbnail;
+        private String sizeHeight;
+        private String sizeWidth;
+    }
+
+}
