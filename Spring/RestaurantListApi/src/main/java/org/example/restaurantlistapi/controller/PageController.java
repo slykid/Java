64eafcb306e706982304b1570e76999dc4e2a58f
slykid <slykid@naver.com>
2024-05-18 07:31:19 +0900

    [Spring] Project: 네이버맛집리스트 API 개발 * View & PageController 추가

--- Spring/RestaurantListApi/src/main/java/org/example/restaurantlistapi/controller/PageController.java
+++ Spring/RestaurantListApi/src/main/java/org/example/restaurantlistapi/controller/PageController.java
@@ -1,15 +1,14 @@
 package org.example.restaurantlistapi.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 @RequestMapping("/pages")
 public class PageController {
 
-    @GetMapping("/main")
     public ModelAndView main() {
         return new ModelAndView("main");
     }
