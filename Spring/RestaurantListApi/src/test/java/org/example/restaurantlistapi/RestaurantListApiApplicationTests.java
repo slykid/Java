1653792b41dbf13d6ad286a4645c29d765a85f15
slykid <slykid@naver.com>
2024-05-18 06:59:06 +0900

    [Spring] Project: 네이버맛집리스트 API 개발 * WishListService, WishListDto 추가 * 컨트롤러(ApiController) 추가 * WishListService, ApiController 테스트 코드 추가

---
+++ Spring/RestaurantListApi/src/test/java/org/example/restaurantlistapi/RestaurantListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.restaurantlistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantListApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
