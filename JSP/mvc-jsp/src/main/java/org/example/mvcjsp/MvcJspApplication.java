50c12d8fd40fb16462bc89bf871b52528dd991e6
slykid <slykid@naver.com>
2024-05-21 12:16:07 +0900

    [Spring] 22. Swagger

---
+++ JSP/mvc-jsp/src/main/java/org/example/mvcjsp/MvcJspApplication.java
@@ -0,0 +1,13 @@
+package org.example.mvcjsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcJspApplication.class, args);
+	}
+
+}
---
+++ Spring/SpringValidationExample/src/test/java/com/example/springvalidationexample/SpringValidationExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springvalidationexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringValidationExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
