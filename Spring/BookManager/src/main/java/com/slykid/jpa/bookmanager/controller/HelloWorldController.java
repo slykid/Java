f9afa9e0c54cde9a4c6d542fc28881c4edc1b8e8
slykid <slykid@naver.com>
2024-05-27 22:13:49 +0900

    [Spring] 23. Spring Data Ⅰ: JPA 소개

---
+++ Spring/BookManager/src/main/java/com/slykid/jpa/bookmanager/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.slykid.jpa.bookmanager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+        return "Hello World!";
+    }
+
+}
