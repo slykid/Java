8c1d7a7248d3f702407eff6fbb190b7e19d52fa0
slykid <slykid@naver.com>
2024-06-11 20:27:17 +0900

    [Spring] 24. Spring Data Ⅱ: Lombok 활용하기

---
+++ Spring/BookManager/src/main/java/com/slykid/jpa/bookmanager/domain/User.java
@@ -0,0 +1,17 @@
+package com.slykid.jpa.bookmanager.domain;
+
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Data // @Getter + @Setter + @ToString + @EqualsAndHashCode + @RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder // Builder 형식으로 객체 생성 + 필드값 주입을 수행함
+public class User {
+    @NonNull
+    private String name;
+    @NonNull
+    private String email;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
---
+++ Spring/BookManager/src/test/java/com/slykid/jpa/bookmanager/domain/UserTest.java
@@ -0,0 +1,26 @@
+package com.slykid.jpa.bookmanager.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+    @Test
+    void test() {
+        User user = new User();
+        user.setEmail("slykid@naver.com");
+        user.setName("slykid");
+
+        System.out.println("User: " + user.getName() + " / Email: " + user.getEmail());
+        System.out.println(user.toString());
+
+        User user1 = new User("slykid", "slykid@naver.com", LocalDateTime.now(), LocalDateTime.now());
+        User user2 = new User("slykid", "slykid@naver.com");
+
+        System.out.println(user1.toString());
+        System.out.println(user2.toString());
+    }
+
+}
