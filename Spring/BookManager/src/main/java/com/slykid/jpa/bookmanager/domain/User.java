2bb1d8c8c1e6d4fba37763fc220505155eda0081
slykid <slykid@naver.com>
2024-06-13 14:12:32 +0900

    [Spring] 26. Spring Data: JPA 다루기 Ⅰ

--- Spring/BookManager/src/main/java/com/slykid/jpa/bookmanager/domain/User.java
+++ Spring/BookManager/src/main/java/com/slykid/jpa/bookmanager/domain/User.java
@@ -7,12 +7,16 @@ import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Data // @Getter + @Setter + @ToString + @EqualsAndHashCode + @RequiredArgsConstructor
 @NoArgsConstructor
 @AllArgsConstructor
 @RequiredArgsConstructor
+@Data // @Getter + @Setter + @ToString + @EqualsAndHashCode + @RequiredArgsConstructor
 @Builder // Builder 형식으로 객체 생성 + 필드값 주입을 수행함
+@Entity  // 데이터베이스의 테이블과 연결하는 객체, 식별을 위한 ID가 필요함
 public class User {
+    @Id                    // 해당 변수를 @Entity 의 ID 값으로 사용
+    @GeneratedValue        // 자동으로 ID 생성을 위한 설정
+    private Long id;
 
     @NonNull
     private String name;
