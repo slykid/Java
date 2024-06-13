c26fb73c415705d1a590e1c31378caea8151a366
slykid <slykid@naver.com>
2024-06-13 13:18:14 +0900

    Rollback

--- Spring/BookManager/src/main/java/com/slykid/jpa/bookmanager/domain/User.java
+++ Spring/BookManager/src/main/java/com/slykid/jpa/bookmanager/domain/User.java
@@ -12,11 +12,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @RequiredArgsConstructor
 @Builder // Builder 형식으로 객체 생성 + 필드값 주입을 수행함
-@Entity  // 선언 시, 식별할 수 있는 객체가 필요함
 public class User {
-    @Id  // @Entity 어노테이션 선언 시, 해당 멤버변수를 Entity의 ID로 사용하겠다는 의미
-    @GeneratedValue  // 자동으로 값을 증가시켜주기위한 조치
-    private Long id;
 
     @NonNull
     private String name;
