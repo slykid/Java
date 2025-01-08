f9c08bc22aad0ee6110fe018c6a3c5c2c70f9cef
slykid <slykid@naver.com>
2025-01-08 16:35:55 +0900

    FIX: [Spring] 2. 웹 개발 기초 * 정적 컨텐츠 vs. MVC vs. API 개발방식 간략 비교 * 실습: 회원 관리 서비스 개발

---
+++ Spring/hello-spring/src/main/java/com/example/hellospring/service/MemberService.java
@@ -0,0 +1,37 @@
+/**
+ * packageName  : com.example.hellospring.service
+ * fileName     : MemberService
+ * author       : slykid
+ * date         : 25. 1. 8.
+ * description
+ * ===================================================
+ * DATE                 AUTHOR              NOTE
+ * ---------------------------------------------------
+ * 25. 1. 8.          slykid         최초작성
+ */
+package com.example.hellospring.service;
+
+import com.example.hellospring.repository.MemberRepository;
+import com.example.hellospring.repository.MemoryMemberRepository;
+
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     * - 같은 이름의 중복 회원은 제
+     */
+    public Long join(Member member) {
+        Optional<Member> result = memberRepository.findByName(member.getName());
+        result.ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+
+        memberRepository.save(member);
+
+        return member.getId();
+    }
+
+}
