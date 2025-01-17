f7eb7a4982ca56a668b314500a49f0b27bf53041
slykid <slykid@naver.com>
2025-01-18 08:38:29 +0900

    FIX: [Spring] 4. 회원 관리 예제 - 웹 MVC 개발 * 홈 화면 추가

---
+++ Spring/hello-spring/src/main/java/com/example/hellospring/controller/HomeController.java
@@ -0,0 +1,26 @@
+/**
+ * packageName  : com.example.hellospring.controller
+ * fileName     : HomeController
+ * author       : kilhyunkim
+ * date         : 25. 1. 18.
+ * description
+ * ===================================================
+ * DATE                 AUTHOR              NOTE
+ * ---------------------------------------------------
+ * 25. 1. 18.          kilhyunkim         최초작성
+ */
+package com.example.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+
+}
