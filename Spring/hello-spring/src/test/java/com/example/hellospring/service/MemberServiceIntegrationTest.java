4f71301cb307387f616598f4e14ba87fa1b0aa65
slykid <slykid@naver.com>
2025-01-18 10:14:56 +0900

    FIX: [Spring] 5. 스프링 DB 접근 기술 * 순수 JDBC를 이용한 DB 접근 테스트

---
+++ Spring/hello-spring/src/test/java/com/example/hellospring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,75 @@
+/**
+ * packageName  : com.example.hellospring.service
+ * fileName     : MemberServiceTest
+ * author       : kilhyunkim
+ * date         : 25. 1. 8.
+ * description
+ * ===================================================
+ * DATE                 AUTHOR              NOTE
+ * ---------------------------------------------------
+ * 25. 1. 8.          kilhyunkim         최초작성
+ */
+package com.example.hellospring.service;
+
+import com.example.hellospring.domain.Member;
+import com.example.hellospring.repository.MemberRepository;
+import com.example.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("hello");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        assertEquals(member.getName(), findMember.getName());
+
+    }
+
+    @Test
+    public void 중복회원_예외() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        /*
+        try {
+            memberService.join(member2);
+            fail();
+        } catch(IllegalStateException e) {
+            assertEquals("이미 존재하는 회원입니다.", e.getMessage())
+        }
+        */
+
+        //then
+        assertEquals("이미 존재하는 회원입니다.", e.getMessage());
+    }
+
+}
