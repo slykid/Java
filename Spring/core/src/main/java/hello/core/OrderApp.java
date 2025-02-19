6a1522d2c906f7d64cf37ac8a2546256d9763418
slykid <slykid@naver.com>
2025-02-19 16:52:42 +0900

    FEAT: [Spring] 8. Spring 핵심원리 Ⅱ: 객체지향원리 적용 * 비율 할인 정책 추가 생성 * 관심사 분리를 위한 AppConfig 및 생성자주입 설정 * 생성자 주입에 따른 관련 소스 코드 및 테스트 코드 수정

--- Spring/core/src/main/java/hello/core/OrderApp.java
+++ Spring/core/src/main/java/hello/core/OrderApp.java
@@ -11,10 +11,8 @@
  */
 package hello.core;
 
-import hello.core.member.Grade;
-import hello.core.member.Member;
-import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.*;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
@@ -22,8 +20,9 @@ import hello.core.order.OrderServiceImpl;
 public class OrderApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
