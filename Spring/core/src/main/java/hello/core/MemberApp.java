696af8787e89371d0becc530f2073d2d45ba9f23
slykid <slykid@naver.com>
2025-03-12 20:42:37 +0900

    FEAT: [Spring] 10. Spring 핵심원리 Ⅳ: Spring으로 전환하기 * ApplicationContext를 활용한 스프링 컨테이너와 스프링 빈으로 객체 생성 및 등록

--- Spring/core/src/main/java/hello/core/MemberApp.java
+++ Spring/core/src/main/java/hello/core/MemberApp.java
@@ -12,15 +12,19 @@
 package hello.core;
 
 import hello.core.member.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
-    public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+    public static void main(String[] args)
+    {
+        // Spring Container & Bean 으로 불러오기
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
-
 
         Member findMember = memberService.findMember(1L);
         System.out.println("new member: " + member.getName());
--- Spring/core/src/main/java/hello/core/OrderApp.java
+++ Spring/core/src/main/java/hello/core/OrderApp.java
@@ -16,13 +16,17 @@ import hello.core.member.*;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
-    public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+    public static void main(String[] args)
+    {
+        // Spring Container & Bean 으로 불러오기
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
@@ -32,7 +36,6 @@ public class OrderApp {
 
         System.out.println("order = " + order);
         System.out.println("calculate price = " + order.calculatePrice());
-
     }
 
 }
